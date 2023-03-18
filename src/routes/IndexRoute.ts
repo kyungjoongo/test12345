54a4394d321b97187d6439426383e1a3ffeb0dc3
kyungjoongo <kyungjoon.go@gmail.com>
2023-03-19 06:35:29 +0900

    asdasdㄴㅇㄹㅎㄴㅇㄹㅎㄴㅇㄹㅎghjkghjkasdfsdf1231231233234

--- src/routes/IndexRoute.ts
+++ src/routes/IndexRoute.ts
@@ -3,13 +3,14 @@ import {keyConfig} from "../config/keyConfig";
 
 //const jwt = require('jsonwebtoken');
 import jwt from 'jsonwebtoken'
+import {httpStatus} from "../config/httpStatus";
 
 const indexRoutes = express.Router();
 
 
 indexRoutes.get("/", async (req: Request, res: Response, next: NextFunction) => {
 
-    return res.render('index', {
+    /*return res.render('index', {
         data : [
             {
                 name: 'kyungjoon',
@@ -24,7 +25,9 @@ indexRoutes.get("/", async (req: Request, res: Response, next: NextFunction) => 
                 age: 46,
             },
         ]
-    })
+    })*/
+
+    return res.status(Number(httpStatus.OK)).json({message:"server health is ok!!"});
 });
 
 
